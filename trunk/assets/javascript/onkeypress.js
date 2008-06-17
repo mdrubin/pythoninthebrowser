eb8b8f4332458bcb1b7510bced6a0767c5c5a55f
fuzzyman <fuzzyman@86879438-a94f-0410-88b6-6f274e2b0fec>
2008-06-17 12:58:12 +0000

    * Allow ctrl-c, z, y, a (and meta modifier) in console * Allow home / end keyboard navigation * Changed font size yet again
    Michael

--- trunk/assets/javascript/onkeypress.js
+++ trunk/assets/javascript/onkeypress.js
@@ -9,19 +9,31 @@ function KeyPress(e){
 				
 	if(window.event) {// IE
 		keynum = e.keyCode;
+		ctrl = e.ctrlKey;
 	}
 	else if(e.which) { // Netscape/Firefox/Opera
 		keynum = e.which;
+		ctrl = e.ctrlKey || e.metaKey;
 	}
 	else {
+	    // unknown browser
 		return true;
 	}
 	
-	if ((keynum >= 37) && (keynum < 41)) {
+	// document.getElementById('debugging').innerHTML = keynum
+	// document.getElementById('debugging2').innerHTML = ctrl
+	// document.getElementById('debugging3').innerHTML = String.fromCharCode(keynum);
+	
+	if ((keynum >= 33) && (keynum < 41)) {
 		return true;
 	}
 	
+	var mask = /[aczyACZY]/;
+
 	keychar = String.fromCharCode(keynum);
+	if (mask.test(keychar) && ctrl) {
+	    return true;
+	} 
 	// element2.innerHTML = 'KeyNum: ' + keynum + ' KeyChar: ' + keychar;
 	retVal = control.Content.onkeypress.method(start, end, keychar);
 	// element.innerHTML = element.innerHTML + ' Result: ' + retVal;
